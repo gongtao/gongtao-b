28c3bcd2e26aa14754c106eb5c60323eeea12804
gongtao <gongtao@jike.com>
2013-12-30 09:36:20 +0800

    Initial Commit

---
+++ BananaNews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BananaNews
+//
+//  Created by 龚涛 on 12/30/13.
+//  Copyright (c) 2013 龚涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BananaNews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BananaNews
+//
+//  Created by 龚涛 on 12/30/13.
+//  Copyright (c) 2013 龚涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
