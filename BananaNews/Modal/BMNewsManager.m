93c8c37c44e2eda5e2136f4bd4f8c54bd0125d55
gongtao <gongtao@jike.com>
2014-04-21 15:42:46 +0800

    修改数据

--- BananaNews/Modal/BMNewsManager.m
+++ BananaNews/Modal/BMNewsManager.m
@@ -598,18 +598,20 @@
 //NewsCategory
 - (NewsCategory *)createNewsCategory:(NSDictionary *)dic context:(NSManagedObjectContext *)context
 {
-    NSNumber *cid = dic[@"category_id"];
+    id cid = dic[@"category_id"];
     
     if (!cid || (NSNull *)cid == [NSNull null]) {
         NSLog(@"NewsCategory: cid null");
         return nil;
     }
     
-    NewsCategory *newsCategory = [self getNewsCategoryById:cid.stringValue context:context];
+    NSString *cidStr = [NSString stringWithFormat:@"%@", cid];
+    
+    NewsCategory *newsCategory = [self getNewsCategoryById:cidStr context:context];
     
     if (!newsCategory) {
         newsCategory = [NSEntityDescription insertNewObjectForEntityForName:NewsCategory_Entity inManagedObjectContext:context];
-        newsCategory.category_id = cid.stringValue;
+        newsCategory.category_id = cidStr;
     }
     
     NSString *name = dic[@"name"];
