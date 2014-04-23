7a2eadb66d441da43d7247aa319f7dc9ef62a96b
zhumanyu <259571082@qq.com>
2014-04-23 16:07:34 +0800

    add file

---
+++ BananaNews/ViewController/BMHistoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMHistoryTableViewController.h
+//  BananaNews
+//
+//  Created by 曼瑜 朱 on 14-4-23.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "GTTableViewController.h"
+
+@interface BMHistoryTableViewController : GTTableViewController
+
+@end
---
+++ BananaNews/ViewController/BMHistoryTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  BMHistoryTableViewController.m
+//  BananaNews
+//
+//  Created by 曼瑜 朱 on 14-4-23.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "BMHistoryTableViewController.h"
+
+@interface BMHistoryTableViewController ()
+
+@end
+
+@implementation BMHistoryTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
