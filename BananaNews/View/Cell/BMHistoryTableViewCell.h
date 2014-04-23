a605fbb806b66b48200bb803f6daaef6cc908bd2
zhumanyu <259571082@qq.com>
2014-04-23 16:09:12 +0800

    add cell

---
+++ BananaNews/View/Cell/BMHistoryTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BMHistoryTableViewCell.h
+//  BananaNews
+//
+//  Created by 曼瑜 朱 on 14-4-23.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMHistoryTableViewCell : UITableViewCell
+
+@end
