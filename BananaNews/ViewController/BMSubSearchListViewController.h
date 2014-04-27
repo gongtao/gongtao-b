1a3785edb66780c70c941f783606a3c63350b4a7
Gong Tao <gongtao@jike.com>
2014-04-27 14:24:09 +0800

    添加文件

---
+++ BananaNews/ViewController/BMSubSearchListViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMSubSearchListViewController.h
+//  BananaNews
+//
+//  Created by 龚 涛 on 14-4-27.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "GTTableViewController.h"
+
+@interface BMSubSearchListViewController : GTTableViewController
+
+@end
